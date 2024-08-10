b6e6347c42156c7bd0b8f3cdf0e978674d51d6cc
shivani <shivaninimmy@gmail.com>
2024-08-10 13:19:11 -0400

    Initial commit

---
+++ src/main/java/com/example/inventorymanagment/InventorymanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inventorymanagment/InventorymanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorymanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
