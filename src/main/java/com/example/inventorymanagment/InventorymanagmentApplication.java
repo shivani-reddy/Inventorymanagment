e721b66a7d4ceaa63e9e5467be516ed85be8ca06
shivani <shivaninimmy@gmail.com>
2024-08-19 10:47:45 -0400

    Task1

--- src/main/java/com/example/inventorymanagment/InventorymanagmentApplication.java
+++ src/main/java/com/example/inventorymanagment/InventorymanagmentApplication.java
@@ -1,9 +1,11 @@
 package com.example.inventorymanagment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class InventorymanagmentApplication {
 
 	public static void main(String[] args) {
