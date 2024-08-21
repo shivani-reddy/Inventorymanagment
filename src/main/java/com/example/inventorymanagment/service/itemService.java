122d7439c176c9b017268b2b1731d82937579f0c
shivani <shivaninimmy@gmail.com>
2024-08-21 12:09:11 -0400

    Task 3

---
+++ src/main/java/com/example/inventorymanagment/service/itemService.java
@@ -0,0 +1,24 @@
+package com.example.inventorymanagment.service;
+
+import com.example.inventorymanagment.model.Item;
+import com.example.inventorymanagment.repo.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class itemService {
+
+    @Autowired
+    private ItemRepository itemRepo;
+
+    public List<Item> getAllItems() {
+        return itemRepo.findAll();
+    }
+
+   // public  Item addItem(Item item) {
+      //  return itemRepo.save(item);
+    //}
+}
